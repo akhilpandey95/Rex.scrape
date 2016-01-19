d5c9618423045fc34a47c02433bd00f1b141d535
AkhilHector <akhilhector.1@gmail.com>
2016-01-19 21:55:29 +0530

    Changes made to the GET call of the API router
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- api.js
+++ api.js
@@ -22,16 +22,28 @@ module.exports.unleash = function() {
         ui.use(b.json());
         ui.use(b.urlencoded({ extended : true}));
         
+        // The REX UI router
         rex_ui.get('/', function(req, res) {
                 res.send("The UI of Rex")
+        });
+
+        rex_ui.get('/:somevalue', function(req, res) {
+                var data = req.params.somevalue;
+                res.send("Sorry cannot GET/" + data + " on this page");
         });
 
         rex_ui.get('/app', function(req, res) {
                 res.send("The main app");
         });
-
+        
+        // The REX API router
         rex_api.get('/', function(req, res) {
                 res.send("The API powering Rex");
+        });
+
+        rex_api.get('/:value', function(req, res) {
+                var data = req.params.value;
+                res.send("You have requested for http://localhost:8000/" + data);
         });
 
         ui.use('/secret', rex_api);
